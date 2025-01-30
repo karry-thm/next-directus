46d0ff2f13339908acc53bdebec47c8fddf3996a
Martin Karry <martin@karry.de>
2025-01-30 23:21:29 +0100

    Dynamic setup (with post)

--- packages/web/src/app/revalidate/route.tsx
+++ packages/web/src/app/revalidate/route.tsx
@@ -1,7 +1,25 @@
 import { revalidatePath } from "next/cache"
 
-export async function GET(request: Request) {
+export async function GET(_request: Request) {
     revalidatePath("/", "page");
     revalidatePath("/", "layout");
-    return Response.json({msg: "OK"})
+    return Response.json({ msg: "OK" })
+}
+
+export async function POST(req: Request) {
+    const {key, keys} = await req.json();
+
+    if(typeof key === "number") {
+        revalidatePath(`/${key}`, "page")
+    }
+
+    if(Array.isArray(keys)) {
+        if(keys.length > 0) {
+            revalidatePath(`/${keys[0]}`, "page")
+        }
+    }
+
+    revalidatePath('/', "layout");
+    
+    return Response.json({msg: "Hallo Welt!"});
 }
